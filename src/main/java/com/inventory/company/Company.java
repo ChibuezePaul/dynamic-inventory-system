7fbcf58ad3a162a09f1fe3f9ad11549320facc9a
chibuezepaul <chibuezepaul2@gmail.com>
2019-05-24 18:38:11 +0100

    Model end (Paul)

--- src/main/java/com/inventory/company/Company.java
+++ src/main/java/com/inventory/company/Company.java
@@ -9,4 +9,46 @@ public class Company {
     private String footerText;
     private String coyDecsription;
     private Timestamp coyCode;
+
+    public Company(String coyLogo, String footerText, String coyDecsription, Timestamp coyCode) {
+        this.coyLogo = coyLogo;
+        this.footerText = footerText;
+        this.coyDecsription = coyDecsription;
+        this.coyCode = coyCode;
+    }
+
+    public Company() {
+    }
+
+    public String getCoyLogo() {
+        return coyLogo;
+    }
+
+    public void setCoyLogo(String coyLogo) {
+        this.coyLogo = coyLogo;
+    }
+
+    public String getFooterText() {
+        return footerText;
+    }
+
+    public void setFooterText(String footerText) {
+        this.footerText = footerText;
+    }
+
+    public String getCoyDecsription() {
+        return coyDecsription;
+    }
+
+    public void setCoyDecsription(String coyDecsription) {
+        this.coyDecsription = coyDecsription;
+    }
+
+    public Timestamp getCoyCode() {
+        return coyCode;
+    }
+
+    public void setCoyCode(Timestamp coyCode) {
+        this.coyCode = coyCode;
+    }
 }
--- src/main/java/com/inventory/supplier/Supplier.java
+++ src/main/java/com/inventory/supplier/Supplier.java
@@ -8,4 +8,27 @@ public class Supplier {
     private String supplierWebsite;
     private Timestamp supplierCode;
 
+    public Supplier(String supplierWebsite, Timestamp supplierCode) {
+        this.supplierWebsite = supplierWebsite;
+        this.supplierCode = supplierCode;
+    }
+
+    public Supplier() {
+    }
+
+    public String getSupplierWebsite() {
+        return supplierWebsite;
+    }
+
+    public void setSupplierWebsite(String supplierWebsite) {
+        this.supplierWebsite = supplierWebsite;
+    }
+
+    public Timestamp getSupplierCode() {
+        return supplierCode;
+    }
+
+    public void setSupplierCode(Timestamp supplierCode) {
+        this.supplierCode = supplierCode;
+    }
 }
