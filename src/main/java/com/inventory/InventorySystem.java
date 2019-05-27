35d4608f09e466c707ab5495ce59499e57ebe575
chibuezepaul <chibuezepaul2@gmail.com>
2019-05-27 13:36:00 +0100

    Name Changed

--- src/main/java/com/inventory/InventorySystem.java
+++ src/main/java/com/inventory/InventorySystem.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
-public class DynamicInventoryApplication {
+public class InventorySystem {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DynamicInventoryApplication.class, args);
+		SpringApplication.run(InventorySystem.class, args);
 	}
 
 }
--- src/test/java/com/inventory/InventorySystemTests.java
+++ src/test/java/com/inventory/InventorySystemTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DynamicInventoryApplicationTests {
+public class InventorySystemTests {
 
 	@Test
 	public void contextLoads() {
